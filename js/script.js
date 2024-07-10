1c6fea855d8a07a15b9be273e74d70ba9724c761
HarleyMartins <harleyfla05@gmail.com>
2024-07-10 16:16:35 -0300

    fix: efeito do header removido

--- js/script.js
+++ js/script.js
@@ -1,27 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var header = document.getElementById("header")
-  var lastScrollPosition = 0
-
-  window.addEventListener("scroll", function () {
-    var scrollPosition = window.scrollY
-
-    // Verifica se o usuário está rolando para baixo
-    if (scrollPosition > lastScrollPosition) {
-      header.style.backgroundColor = "#1f1f1f" // Preto
-      header.style.padding = ""
-    } else {
-      // Volta à cor original quando atingir o topo
-      if (scrollPosition === 0) {
-        header.style.backgroundColor = ""
-        header.style.padding = ""
-      }
-    }
-
-    // Atualiza a posição de rolagem
-    lastScrollPosition = scrollPosition
-  })
-})
-
 const butttonDropdown = document
   .getElementById("btnDropdown")
   .addEventListener("click", showDropdown)
