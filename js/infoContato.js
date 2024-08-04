73c2ca6a16d2331b8a278a9176595726be2f55e1
HarleyMartins <harleyfla05@gmail.com>
2024-08-03 22:15:07 -0300

    feat: enviar informações para o whatsapp adicionado

---
+++ js/infoContato.js
@@ -0,0 +1,23 @@
+const botaoEnviaContato = document.getElementById("botaoEnviarContato")
+
+function capturaInformacoes(ev) {
+  ev.preventDefault()
+  let name = document.getElementById("name").value
+  let email = document.getElementById("email").value
+  let mensagem = document.getElementById("mensagem").value
+  console.log(name, email, mensagem)
+
+  enviaMensagem(name, email, mensagem)
+}
+
+botaoEnviaContato.addEventListener("click", capturaInformacoes)
+
+function enviaMensagem(name, email, mensagem) {
+  const informacoes = `${name} \n ${email} \n ${mensagem}`
+  const notificacao = "NOVA MENSAGEM"
+  const mensagemCodificada = encodeURI(informacoes)
+  const phone = "+5588998415485"
+  window.open(
+    `https://wa.me/${phone}?text=${notificacao}%0A ${mensagemCodificada}`
+  )
+}
