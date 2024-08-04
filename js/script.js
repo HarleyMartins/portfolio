97f6d2e35414e633b2d4b719a440015ab7dffc41
HarleyMartins <harleyfla05@gmail.com>
2024-08-03 21:19:48 -0300

    fix: funçõe do tema claro ocultadas

--- js/script.js
+++ js/script.js
@@ -1,91 +1,91 @@
-const butttonDropdown = document
-  .getElementById("btnDropdown")
-  .addEventListener("click", showDropdown)
+// const butttonDropdown = document
+//   .getElementById("btnDropdown")
+//   .addEventListener("click", showDropdown)
 
-function showDropdown(ev) {
-  ev.preventDefault()
-  const dropdownContent = document.getElementById("dropdown-content")
-  dropdownContent.classList.toggle("open")
-}
+// function showDropdown(ev) {
+//   ev.preventDefault()
+//   const dropdownContent = document.getElementById("dropdown-content")
+//   dropdownContent.classList.toggle("open")
+// }
 
-const buttonLight = document
-  .getElementById("btnLight")
-  .addEventListener("click", modoClaro)
+// const buttonLight = document
+//   .getElementById("btnLight")
+//   .addEventListener("click", modoClaro)
 
-function modoClaro() {
-  header.style.backgroundColor = "#FFFF"
-  var h1 = document.querySelectorAll("h1").forEach((item) => {
-    item.style.color = "#000"
-  })
-  var h2 = document.querySelectorAll("h2").forEach((item) => {
-    item.style.color = "#000"
-  })
+// function modoClaro() {
+//   header.style.backgroundColor = "#FFFF"
+//   var h1 = document.querySelectorAll("h1").forEach((item) => {
+//     item.style.color = "#000"
+//   })
+//   var h2 = document.querySelectorAll("h2").forEach((item) => {
+//     item.style.color = "#000"
+//   })
 
-  var h3 = document.querySelectorAll("h3").forEach((item) => {
-    item.style.color = "#000"
-  })
+//   var h3 = document.querySelectorAll("h3").forEach((item) => {
+//     item.style.color = "#000"
+//   })
 
-  var p = document.querySelectorAll("p").forEach((item) => {
-    item.style.color = "#000"
-  })
+//   var p = document.querySelectorAll("p").forEach((item) => {
+//     item.style.color = "#000"
+//   })
 
-  var headerLinks = document
-    .querySelectorAll(".header__links__link")
-    .forEach((item) => {
-      item.style.color = "#000"
-    })
+//   var headerLinks = document
+//     .querySelectorAll(".header__links__link")
+//     .forEach((item) => {
+//       item.style.color = "#000"
+//     })
 
-  var sectionInicio = document.getElementById("inicio")
-  sectionInicio.style.backgroundColor = "#f4fcff"
+//   var sectionInicio = document.getElementById("inicio")
+//   sectionInicio.style.backgroundColor = "#f4fcff"
 
-  var sectionSobre = document.getElementById("sobre")
-  sectionSobre.style.backgroundColor = "#FFFFFF"
+//   var sectionSobre = document.getElementById("sobre")
+//   sectionSobre.style.backgroundColor = "#FFFFFF"
 
-  var sectionProjetos = document.getElementById("projetos")
-  sectionProjetos.style.backgroundColor = "#f4fcff"
+//   var sectionProjetos = document.getElementById("projetos")
+//   sectionProjetos.style.backgroundColor = "#f4fcff"
 
-  var sectionContato = document.getElementById("contato")
-  sectionContato.style.backgroundColor = "#FFFFFF"
+//   var sectionContato = document.getElementById("contato")
+//   sectionContato.style.backgroundColor = "#FFFFFF"
 
-  var footer = document.querySelector("footer")
-  footer.style.backgroundColor = "#f9fdff"
+//   var footer = document.querySelector("footer")
+//   footer.style.backgroundColor = "#f9fdff"
 
-  var inicioCard = document.querySelector(".inicio__card")
-  inicioCard.style.backgroundColor = "#FFF"
-  inicioCard.style.border = "0.5px solid #66c190"
+//   var inicioCard = document.querySelector(".inicio__card")
+//   inicioCard.style.backgroundColor = "#FFF"
+//   inicioCard.style.border = "0.5px solid #66c190"
 
-  var cardProjetos = document
-    .querySelectorAll(".projetos__cards__card")
-    .forEach((item) => {
-      item.style.backgroundColor = "#FFFF"
-      item.style.border = "none"
-    })
+//   var cardProjetos = document
+//     .querySelectorAll(".projetos__cards__card")
+//     .forEach((item) => {
+//       item.style.backgroundColor = "#FFFF"
+//       item.style.border = "none"
+//     })
 
-  var formularioContato = document.querySelector(".formulario")
-  formularioContato.style.backgroundColor = "#f4fcff"
+//   var formularioContato = document.querySelector(".formulario")
+//   formularioContato.style.backgroundColor = "#f4fcff"
 
-  var formularioLabel = document.querySelectorAll("label").forEach((item) => {
-    item.style.color = "#000"
-  })
+//   var formularioLabel = document.querySelectorAll("label").forEach((item) => {
+//     item.style.color = "#000"
+//   })
 
-  var formularioInput = document
-    .querySelectorAll(".formulario__name > input, .formulario__email > input")
-    .forEach((item) => {
-      item.style.backgroundColor = "#FFFFFF"
-      item.style.border = "none"
-      item.style.color = "#000"
-    })
+//   var formularioInput = document
+//     .querySelectorAll(".formulario__name > input, .formulario__email > input")
+//     .forEach((item) => {
+//       item.style.backgroundColor = "#FFFFFF"
+//       item.style.border = "none"
+//       item.style.color = "#000"
+//     })
 
-  var formularioTextarea = document.querySelector("textarea")
-  formularioTextarea.style.background = "#FFF"
-  formularioTextarea.style.border = "none"
+//   var formularioTextarea = document.querySelector("textarea")
+//   formularioTextarea.style.background = "#FFF"
+//   formularioTextarea.style.border = "none"
 
-  var footerRedes = document
-    .querySelectorAll(".footer__content__redes__a > i")
-    .forEach((item) => {
-      item.style.color = "#000"
-    })
+//   var footerRedes = document
+//     .querySelectorAll(".footer__content__redes__a > i")
+//     .forEach((item) => {
+//       item.style.color = "#000"
+//     })
 
-  var formularioButton = document.querySelector(".formulario__submitbtn")
-  formularioButton.style.backgroundColor = "#66c190"
-}
+//   var formularioButton = document.querySelector(".formulario__submitbtn")
+//   formularioButton.style.backgroundColor = "#66c190"
+// }
